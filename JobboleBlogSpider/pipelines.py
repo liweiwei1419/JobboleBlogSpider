1a8a5d944bbebd543148c59321bee6fc533bd219
李威 <121088825@qq.com>
2018-12-21 17:35:54 +0800

    add pipeline to download images.

--- JobboleBlogSpider/pipelines.py
+++ JobboleBlogSpider/pipelines.py
@@ -4,8 +4,26 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.pipelines.images import ImagesPipeline
 
 
-class JobboleblogspiderPipeline(object):
+class ArticlespiderPipeline(object):
+
     def process_item(self, item, spider):
+        # 重写该方法可以从 results 中获取图片的真实下载地址
+
+        return item
+
+
+class ArticleImagePipeline(ImagesPipeline):
+    """
+    继承 ImagesPipeline 这个类，不要忘记了
+    """
+    # 理解这个方法，我个人感觉像一个拦截器，把 item 拦截下来，去做一些操作
+    def item_completed(self, results, item, info):
+        print(1)
+        if "front_image_url" in item:
+            for ok, value in results:
+                image_file_path = value["path"]
+                item["front_image_path"] = image_file_path
         return item
--- JobboleBlogSpider/settings.py
+++ JobboleBlogSpider/settings.py
@@ -9,82 +9,93 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
+import os
+
 BOT_NAME = 'JobboleBlogSpider'
 
 SPIDER_MODULES = ['JobboleBlogSpider.spiders']
 NEWSPIDER_MODULE = 'JobboleBlogSpider.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'JobboleBlogSpider (+http://www.yourdomain.com)'
+# USER_AGENT = 'JobboleBlogSpider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'JobboleBlogSpider.middlewares.JobboleblogspiderSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'JobboleBlogSpider.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'JobboleBlogSpider.pipelines.JobboleblogspiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    # 设置下载图片的 pipeline
+    # 'scrapy.pipelines.images.ImagesPipeline': 1,
+    'JobboleBlogSpider.pipelines.ArticleImagePipeline': 100,
+    'JobboleBlogSpider.pipelines.ArticlespiderPipeline': 300,
+}
+
+# 获取项目目录
+project_dir = os.path.abspath(os.path.dirname(__file__))
+# 指定需要下载的图片字段，这个字段必须是可迭代对象，这里 front_img_url 就是图片字段名
+IMAGES_URLS_FIELD = "front_image_url"
+# 指定图片存储路径，这里将项目目录与 images 目录进行拼接，作为图片存储路径
+IMAGES_STORE = os.path.join(project_dir, 'images')
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
